b636c90444f97142aaeed28ba7b4dc209e3f4a91
fpasovic <fpasovic@calliduscloud.com>
2016-09-19 22:54:43 +0200

    Adding files for assignment1

---
+++ myApp.js
@@ -0,0 +1,12 @@
+(function(){
+'use srtrict';
+angular.module("MyFirstApp",[])
+.controller("myFirstController",[function(){
+
+var self = this;
+self.firstname = 'Fikret';
+//$scope.firstname = 'Fikret';
+
+}]);
+
+})();
