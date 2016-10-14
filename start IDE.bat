8411e706995f7e1969e3c361cc009e376efe1a12
fpasovic <fpasovic@calliduscloud.com>
2016-10-14 13:20:55 +0200

    start ide change

--- start IDE.bat
+++ start IDE.bat
@@ -1,6 +1,7 @@
 cd d:
-cd d:\Dropbox\Citaliste\COURSERA\Single Page web apps with angularjs\git\fullstack-course5\examples\Lecture02\
-atom .
-
+cd d:\Dropbox\Citaliste\COURSERA\Single Page web apps with angularjs\git\fullstack-course5\assignments\assignment2\assignment2-starter-code\
+pause
+start atom . /wait
+echo Browser-sync is about to be started
 browser-sync start --server --directory --files "**/*"
 pause
